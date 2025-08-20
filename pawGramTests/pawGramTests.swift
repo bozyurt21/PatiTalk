282d872ea275c9ff0502f043e0c4a10a8c87afcc
bozyurt21 <bozyurt21@ku.edu.tr>
2025-08-21 02:17:03 +0300

    Initial Commit

---
+++ pawGramTests/pawGramTests.swift
@@ -0,0 +1,17 @@
+//
+//  pawGramTests.swift
+//  pawGramTests
+//
+//  Created by Bensu Özyurt on 21.08.2025.
+//
+
+import Testing
+@testable import pawGram
+
+struct pawGramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
