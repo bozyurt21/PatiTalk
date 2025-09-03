94d3d690a1108d46d7cd8fcf95c8c2db008603c7
bozyurt21 <bozyurt21@ku.edu.tr>
2025-09-04 01:01:52 +0300

    Added new controllers and changed the UI for better view

---
+++ pawGram/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  patiTalk
+//
+//  Created by Bensu Özyurt on 4.09.2025.
+//
+
+struct K {
+    static let toLoginIdentifier = "toLogin"
+    static let toRegisterIdentifier = "toRegister"
+    static let loginIdentifier = "LoginToMain"
+    static let registerIdentifier = "RegisterToMain"
+}
--- pawGram/Controllers/ViewController.swift
+++ pawGram/Controllers/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 25
+        registerButton.layer.cornerRadius = 25
+        
     }
-
-
 }
 
