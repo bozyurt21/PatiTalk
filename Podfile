7eeb7fcba551dc64124045ebf787dbe04239a891
bozyurt21 <bozyurt21@ku.edu.tr>
2025-09-04 01:11:27 +0300

    Added pod file for third party library usage

---
+++ Podfile
@@ -0,0 +1,6 @@
+
+platform :ios, '13.0'
+
+target 'patiTalk' do
+  use_frameworks!
+end
---
+++ Pods/Target Support Files/Pods-patiTalk/Pods-patiTalk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_patiTalk : NSObject
+@end
+@implementation PodsDummy_Pods_patiTalk
+@end
